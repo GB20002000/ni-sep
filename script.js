caa2064edf1ec5b93a7b567f2ffe73898cc35759
NhidheesKB <nhidhees@gmail.com>
2025-03-13 18:40:54 +0530

    new

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const toggles = document.querySelectorAll(".toggle");
 const count = document.getElementById("count");
-const sdt = document.getElementById("std");
+const room1 = document.getElementById("room1");
+const ucount =document.getElementById("ucount");
+const billcount= document.getElementById("billcount");
+const room2=document.getElementById("room2");
 
 const broker = "wss://53226e2c2b854db5b2ead0d6a5a8d1f3.s1.eu.hivemq.cloud:8884/mqtt";
 const options = {
@@ -15,13 +18,59 @@ const client = mqtt.connect(broker, options);
 client.on("connect", () => {
     console.log("Connected to HiveMQ Cloud");
     client.subscribe("Switch");
+    client.subscribe("Switch2");
+    client.subscribe("Switch3");
+    client.subscribe("Alert1");
+    client.subscribe("Alert2");
 });
 client.on("message", (topic, message) => {
     try {
         const data = JSON.parse(message.toString());
+        const data1 =JSON.parse(message.toString());
+        const data2 = JSON.parse(message.toString());
+        const data3 = JSON.parse(message.toString());
+        const data4 = JSON.parse(message.toString());
         if (topic === "Switch") {
             count.innerHTML = `${data.voltage} ${data.Vunit}`;
         }
+        else if(topic ==="Switch2")
+        {
+            ucount.innerHTML = `${data1.voltage} ${data.Vunit}`;
+        }
+        else if(topic ==="Switch3")
+        {
+            billcount.innerHTML = `${data2.voltage} ${data.Vunit}`;
+        }
+        else if(topic ==="Alert1")
+        {   
+            if(data3.voltage ===1)
+            {
+                room1.innerHTML="Person Dected";
+                room1.style.backgroundColor="blue"
+            }
+            else if(data3.voltage ===0)
+            {
+                room1.innerHTML="Person UNDected";
+                room1.style.backgroundColor="red";
+            }
+
+            
+        }
+        else if(topic ==="Alert2")
+        {
+            if(data4.voltage ===1)
+                {
+                    room2.innerHTML="Person Dected";
+                    room2.style.backgroundColor="blue";
+                }
+                else if(data4.voltage ===0)
+                {
+                    room2.innerHTML="Person UNDected";
+                    room2.style.backgroundColor="red";
+                }
+        }
+
+        
     } catch (error) {
         console.log("Received:", message.toString());
         console.error(error);
